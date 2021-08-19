d162623f641abcacee7376f7f4bf216c4af678cd
DanielSTS <daniel.junior.ifpb@gmail.com>
2021-08-19 00:38:15 -0300

    Ajustando queue

--- settings.py
+++ settings.py
@@ -4,4 +4,3 @@ CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
 MONGO_URI = os.environ.get('MONGO_URI')
 DB = os.environ.get('DB')
-
